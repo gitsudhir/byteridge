f02c7d84eae3d243d2c3c5f5b30a4d53064b439b
Sudhir Kumar <31241120+gitsudhir@users.noreply.github.com>
2023-01-24 05:45:11 -0800

    file added

---
+++ users/users.controller.js
@@ -0,0 +1,78 @@
+﻿const express = require('express');
+const router = express.Router();
+const userService = require('./user.service');
+
+// routes
+router.post('/authenticate', authenticate);
+router.post('/register', register);
+router.get('/', getAll);
+router.get('/audit', getAllAudit);
+router.get('/current', getCurrent);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.delete('/:id', _delete);
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+    userService.authenticate(req.body)
+        .then(user => {
+            
+            if(user) {
+                console.log({user})
+                let login={
+                    login: + new Date(),
+                    ip : req.ip
+                } 
+                console.log({login});
+                userService.update(user._id, Object.assign(user,login))
+                .then(() => res.json(user))
+                .catch(err => {console.log({err});next(err)});
+                
+            }else{
+                res.status(400).json({ message: 'Username or password is incorrect' })
+            }
+        })
+        .catch(err => next(err));
+}
+
+function register(req, res, next) {
+    userService.create(req.body)
+        .then(() => res.json({message:`${req.body.username} is successfully created.`}))
+        .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(err => next(err));
+}
+function getAllAudit(req, res, next) {
+    userService.getAllAudit('audit')
+        .then(users => res.json(users))
+        .catch(err => next(err));
+}
+
+function getCurrent(req, res, next) {
+    userService.getById(req.user.sub)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function update(req, res, next) {
+    userService.update(req.params.id, req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
